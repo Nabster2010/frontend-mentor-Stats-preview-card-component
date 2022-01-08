39c21245799ad5759965ae725742c65f44a3cba4
nabster2010 <nabster201037@gmail.com>
2022-01-08 23:33:33 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "hsl(277, 64%, 61%)",
+        card: "hsl(244, 38%, 16%)",
+        darkBackground: "hsl(233, 47%, 7%)",
+        white: "hsl(0, 0%, 100%)",
+        transparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        SlightlyTransparentWhite: "hsla(0, 0%, 100%, 0.6)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lexendDeca: ["Lexend Deca", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
